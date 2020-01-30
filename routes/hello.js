408fbdf4fe0fe3ca40ab26a533208e8d9b0384ae
Nhu Luong <nhuluong@Nhus-MacBook-Pro.local>
2020-01-30 13:19:01 -0800

    final submission

--- routes/hello.js
+++ routes/hello.js
@@ -4,6 +4,11 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+  console.log("name is " + nameToShow);
+  res.render("hello", {
+  	"name": nameToShow
+  });
   res.render('index', {
   	'name': '',
   });
